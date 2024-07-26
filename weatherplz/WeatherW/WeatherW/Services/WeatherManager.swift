59f156944af4145da6ceb1e686d9d492c327112b
Joel Beilis <joel.beilis@gmail.com>
2024-07-26 16:34:37 -0400

    Imported weatherkit with forecast

---
+++ weatherplz/WeatherW/WeatherW/Services/WeatherManager.swift
@@ -0,0 +1,39 @@
+//
+//  WeatherManager.swift
+//  WeatherW
+//
+//  Created by Joel Beilis on 2024-07-26.
+//
+
+import Foundation
+import WeatherKit
+import CoreLocation
+
+class WeatherManager {
+    static let shared = WeatherManager()
+    let service = WeatherService.shared
+    
+    var temperatureFormatter: MeasurementFormatter = {
+        let formatter = MeasurementFormatter()
+        formatter.numberFormatter.maximumFractionDigits = 0
+        return formatter
+    }()
+    
+    func currentWeather(for location: CLLocation) async -> CurrentWeather? {
+        let currentWeather = await Task.detached(priority: .userInitiated) {
+            let forecast = try? await self.service.weather(
+                for: location,
+                including: .current
+            )
+            return forecast
+        }.value
+        return currentWeather
+    }
+    
+    func weatherAttribution() async -> WeatherAttribution? {
+        let attribution = await Task(priority: .userInitiated) {
+            return try? await self.service.attribution
+        }.value
+        return attribution
+    }
+}
--- weatherplz/WeatherW/WeatherW/WeatherWApp.swift
+++ weatherplz/WeatherW/WeatherW/WeatherWApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WeatherWApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ForecastView()
         }
     }
 }
