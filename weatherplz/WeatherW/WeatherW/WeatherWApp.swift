9af86161cebeffad34c0458cc5aed74fd40637b6
Joel Beilis <joel.beilis@gmail.com>
2024-07-26 15:18:52 -0400

    FIRST REPO IT WORKS YAY

---
+++ weatherplz/WeatherW/WeatherW/WeatherWApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWApp.swift
+//  WeatherW
+//
+//  Created by Joel Beilis on 2024-07-26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
