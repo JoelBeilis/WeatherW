ae3729026b656f6b024e5607835e799a335ab83d
Joel Beilis <joel.beilis@gmail.com>
2024-07-26 18:00:30 -0400

    Renamed icons in alignment with Apple SF

--- weatherplz/WeatherW/WeatherW/ForecastView.swift
+++ weatherplz/WeatherW/WeatherW/ForecastView.swift
@@ -23,14 +23,14 @@ struct ForecastView: View {
                 if let currentWeather {
                     Text(Date.now.formatted(date: .abbreviated, time: .omitted))
                     Text(Date.now.formatted(date: .omitted, time: .shortened))
-                    Image(systemName: currentWeather.symbolName)
-                        .renderingMode(.original)
-                        .symbolVariant(.fill)
-                        .font(.system(size: 60.0, weight: .bold))
+                    Image(currentWeather.symbolName) // Changed from systemName to name
+                        .resizable() // Make the image resizable
+                        .aspectRatio(contentMode: .fit) // Maintain aspect ratio
+                        .frame(width: 60, height: 60) // Set the size of the image
                         .padding()
                         .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(.secondary.opacity(0.2))
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.secondary.opacity(0.2))
                         )
                     let temp = weatherManager.temperatureFormatter.string(from: currentWeather.temperature)
                     Text(temp)
@@ -45,9 +45,9 @@ struct ForecastView: View {
         .task {
             isLoading = true
             Task.detached { @MainActor in
-                currentWeather = await weatherManager.currentWeather(for: CLLocation(latitude: 49.320991, longitude: -123.074718))
+                currentWeather = await weatherManager.currentWeather(for: CLLocation(latitude: 43.856951, longitude: -79.471330))
+                isLoading = false // Move this inside the Task to ensure it gets updated after the data is fetched
             }
-            isLoading = false
         }
     }
 }
