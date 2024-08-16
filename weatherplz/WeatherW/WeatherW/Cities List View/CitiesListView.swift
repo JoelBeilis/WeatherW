865d0f9509f8a86ba408c5b01438676a2450f09b
Joel Beilis <joel.beilis@gmail.com>
2024-08-16 19:50:41 -0400

    Rearranging Cities Works

--- weatherplz/WeatherW/WeatherW/Cities List View/CitiesListView.swift
+++ weatherplz/WeatherW/WeatherW/Cities List View/CitiesListView.swift
@@ -53,6 +53,7 @@ struct CitiesListView: View {
                                             Image(systemName: "trash")
                                         }
                                     }
+                                
                                     .onTapGesture {
                                         selectedCity = city
                                         dismiss()
